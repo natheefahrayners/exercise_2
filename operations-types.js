9d0be630ec59e1c4118240a71ddf996b61fe3866
natheefahrayners <natheefahrayners4@gmail.com>
2025-10-31 09:21:24 +0200

    exerise_2

---
+++ operations-types.js
@@ -0,0 +1,12 @@
+let number1 = 15;
+let number2 = 30;
+
+let numberString = number1.toString();
+console.log(numberString);
+
+let sum = number1 + number2;
+console.log(sum);
+
+let product = number1 * number2;
+console.log(prodcut);
+
